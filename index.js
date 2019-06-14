8b087da4a36de1d07eda0795bbf2cb3999a15a2d
Theodor Gherzan <theodor@balena.io>
2019-06-14 09:52:07 +0100

    Use resin-semver instead of semver
    Signed-off-by: Theodor Gherzan <theodor@balena.io>

--- index.js
+++ index.js
@@ -1,181 +1,133 @@
 "use strict";
 ///<reference path="./typings.d.ts"/>
 var semverRegex = require("semver-regex"); // used for the string formats
-var semver = require("semver"); // used for everything else
+var semver = require("resin-semver"); // used for everything else
 module.exports = function (ajv) {
-    ajv.addFormat("semver", semverRegex);
-    ajv.addKeyword("semver", {
+    ajv.addFormat('semver', semverRegex);
+    ajv.addKeyword('semver', {
         modifying: true,
         compile: function (schema, par, it) {
             var _method;
             if (typeof schema === 'boolean') {
-                return (function (data) { return semver.valid(data) !== null; });
+                return function (data) {
+                    return semver.valid(data) !== null;
+                };
             }
             else if (schema.valid !== undefined) {
-                return (function (data) { return semver.valid(data, schema.loose || false) !== null; });
-            }
-            else if (schema.validRange !== undefined) {
-                return (function (data) { return semver.validRange(data, schema.loose || false) !== null; });
+                return function (data) {
+                    return semver.valid(data) !== null;
+                };
             }
             else if (schema.prerelease !== undefined) {
-                return (function (data) { return semver.prerelease(data, schema.loose || false) !== null; });
+                return function (data) {
+                    return semver.prerelease(data) !== null;
+                };
                 // MODIFYING KEYWORDS
             }
             else if (schema.major !== undefined) {
-                _method = "major";
+                _method = 'major';
             }
             else if (schema.minor !== undefined) {
-                _method = "minor";
+                _method = 'minor';
             }
             else if (schema.patch !== undefined) {
-                _method = "patch";
-            }
-            else if (schema.clean !== undefined) {
-                _method = "clean";
+                _method = 'patch';
                 // RELATIONAL KEYWORDS
             }
             else if (schema.satisfies !== undefined) {
-                _method = "satisfies";
+                _method = 'satisfies';
             }
             else if (schema.gt !== undefined) {
-                _method = "gt";
+                _method = 'gt';
             }
             else if (schema.gte !== undefined) {
-                _method = "gte";
+                _method = 'gte';
             }
             else if (schema.lt !== undefined) {
-                _method = "lt";
+                _method = 'lt';
             }
             else if (schema.lte !== undefined) {
-                _method = "lte";
-            }
-            else if (schema.eq !== undefined) {
-                _method = "eq";
-            }
-            else if (schema.neq !== undefined) {
-                _method = "neq";
-            }
-            else if (schema.ltr !== undefined) {
-                _method = "ltr";
-            }
-            else if (schema.gtr !== undefined) {
-                _method = "gtr";
+                _method = 'lte';
             }
             else {
-                throw new Error("Schema Error: this should be prevented by the metaSchema. Got schema:" + JSON.stringify(schema));
+                throw new Error('Schema Error: this should be prevented by the metaSchema. Got schema:' +
+                    JSON.stringify(schema));
             }
             var out;
             switch (_method) {
-                case "major":
-                case "minor":
-                case "patch":
-                case "clean": {
-                    // MODIFYING KEYWORDS
-                    var _inst;
-                    if ((schema[_method].$data)) {
-                        _inst = it.util.getData(schema[_method].$data, it.dataLevel, it.dataPathArr);
-                    }
-                    else {
-                        _inst = "inst";
-                    }
-                    out = Function("inst", "path", "parent", "prop_name", "data", "try{parent[prop_name] = this.semver." + _method + "(" + _inst + ",this.loose);}catch(err){ return false; }; return true;");
-                    break;
-                }
-                case "satisfies":
-                case "eq":
-                case "lte":
-                case "gte":
-                case "ltr":
-                case "gtr":
-                    {
-                        // RELATIONAL (RANGE) KEYWORDS
-                        var _data = ((schema[_method].$data)
-                            ? it.util.getData(schema[_method].$data, it.dataLevel, it.dataPathArr)
-                            : "\"" + schema[_method] + "\"");
-                        out = Function("inst", "path", "parent", "prop_name", "data", "if(this.semver.validRange(" + _data + ",this.loose)===null){return false;}if(this.semver.validRange(inst,this.loose)===null){return false;};return this.semver." + _method + "(inst," + _data + ",this.loose  );");
-                    }
-                    break;
+                case 'major':
+                case 'minor':
+                case 'patch':
+                case 'satisfies':
+                case 'lte':
+                case 'gte':
                 default: {
                     // RELATIONAL KEYWORDS
-                    var _data = ((schema[_method].$data)
+                    var _data = schema[_method].$data
                         ? it.util.getData(schema[_method].$data, it.dataLevel, it.dataPathArr)
-                        : "\"" + schema[_method] + "\"");
-                    out = Function("inst", "path", "parent", "prop_name", "data", "if(this.semver.valid(" + _data + ")===null){return false;}if(this.semver.valid(inst)===null){return false;};return this.semver." + _method + "(inst," + _data + ",this.loose  );");
+                        : "\"" + schema[_method] + "\"";
+                    out = Function('inst', 'path', 'parent', 'prop_name', 'data', "if(this.semver.valid(" + _data + ")===null){return false;}if(this.semver.valid(inst)===null){return false;};return this.semver." + _method + "(inst," + _data + ");");
                 }
             }
             return out.bind({
-                "semver": semver,
-                "loose": (schema.loose || false)
+                semver: semver
             });
         },
         metaSchema: {
             oneOf: [
-                { type: "boolean" },
+                { type: 'boolean' },
                 {
-                    type: "object",
+                    type: 'object',
                     properties: {
-                        major: { $ref: "#/bool_or_ref" },
-                        minor: { $ref: "#/bool_or_ref" },
-                        patch: { $ref: "#/bool_or_ref" },
-                        clean: { $ref: "#/bool_or_ref" },
-                        satisfies: { $ref: "#/string_or_ref" },
-                        gt: { $ref: "#/string_or_ref" },
-                        gte: { $ref: "#/string_or_ref" },
-                        lt: { $ref: "#/string_or_ref" },
-                        lte: { $ref: "#/string_or_ref" },
-                        eq: { $ref: "#/string_or_ref" },
-                        neq: { $ref: "#/string_or_ref" },
-                        ltr: { $ref: "#/string_or_ref" },
-                        gtr: { $ref: "#/string_or_ref" },
-                        valid: { type: "boolean" },
-                        validRange: { type: "boolean" },
-                        prerelease: { type: "boolean" },
-                        loose: { type: "boolean" },
+                        major: { $ref: '#/bool_or_ref' },
+                        minor: { $ref: '#/bool_or_ref' },
+                        patch: { $ref: '#/bool_or_ref' },
+                        satisfies: { $ref: '#/string_or_ref' },
+                        gt: { $ref: '#/string_or_ref' },
+                        gte: { $ref: '#/string_or_ref' },
+                        lt: { $ref: '#/string_or_ref' },
+                        lte: { $ref: '#/string_or_ref' },
+                        valid: { type: 'boolean' },
+                        prerelease: { type: 'boolean' }
                     },
                     oneOf: [
-                        { required: ["major"] },
-                        { required: ["minor"] },
-                        { required: ["patch"] },
-                        { required: ["clean"] },
-                        { required: ["satisfies"] },
-                        { required: ["validRange"] },
-                        { required: ["gt"] },
-                        { required: ["gte"] },
-                        { required: ["lt"] },
-                        { required: ["lte"] },
-                        { required: ["eq"] },
-                        { required: ["neq"] },
-                        { required: ["ltr"] },
-                        { required: ["gtr"] },
-                        { required: ["valid"] },
-                        { required: ["prerelease"] },
-                    ],
-                },
+                        { required: ['major'] },
+                        { required: ['minor'] },
+                        { required: ['patch'] },
+                        { required: ['satisfies'] },
+                        { required: ['gt'] },
+                        { required: ['gte'] },
+                        { required: ['lt'] },
+                        { required: ['lte'] },
+                        { required: ['valid'] },
+                        { required: ['prerelease'] }
+                    ]
+                }
             ],
             bool_or_ref: {
                 oneOf: [
-                    { type: "boolean" },
+                    { type: 'boolean' },
                     {
-                        type: "object",
+                        type: 'object',
                         properties: {
-                            "$data": { type: "string" }
+                            $data: { type: 'string' }
                         },
-                        required: ["$data"],
-                        maxProperties: 1,
-                    },
+                        required: ['$data'],
+                        maxProperties: 1
+                    }
                 ]
             },
             string_or_ref: {
                 oneOf: [
-                    { type: "string" },
+                    { type: 'string' },
                     {
-                        type: "object",
+                        type: 'object',
                         properties: {
-                            "$data": { type: "string" }
+                            $data: { type: 'string' }
                         },
-                        required: ["$data"],
-                        maxProperties: 1,
-                    },
+                        required: ['$data'],
+                        maxProperties: 1
+                    }
                 ]
             }
         }
